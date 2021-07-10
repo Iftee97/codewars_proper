7421a98998d491ea624fc4903e30d76f91ef5316
Iftee1711402 <tawsifhaque97@gmail.com>
2021-07-11 04:52:54 +0600

    ...

--- 8 kyu/Playing with cubes II/app.js
+++ 8 kyu/Playing with cubes II/app.js
@@ -22,3 +22,18 @@ class Cube {
         return `Should be ${this.side}`;
     }
 }
+
+// solution-2:
+class Cube {
+    constructor(side) {
+        this.setSide(side)
+    }
+
+    getSide() {
+        return this.side
+    }
+
+    setSide(side = 0) {
+        this.side = Math.abs(side)
+    }
+}
