2a6aa27221d2aebc5bd75af68475f0b51cc8f7cd
Iftee1711402 <tawsifhaque97@gmail.com>
2021-09-02 06:53:43 +0600

    ...

---
+++ 8 kyu/Well of Ideas - Easy Version/app.js
@@ -0,0 +1,25 @@
+/*
+
+Well of Ideas - Easy Version
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+// solution:
+function well(x) {
+    let goodCount = 0;
+    for (let goodOrBad of x) {
+        if (goodOrBad === 'good')
+            goodCount += 1;
+    }
+
+    if (goodCount === 0)
+        return 'Fail!';
+    else if (goodCount === 1 || goodCount === 2)
+        return 'Publish!';
+    else if (goodCount > 2)
+        return 'I smell a series!';
+}
