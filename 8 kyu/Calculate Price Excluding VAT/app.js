3e967a76975d6606f3760f7369904e9382985502
Iftee1711402 <tawsifhaque97@gmail.com>
2021-09-19 00:48:10 +0600

    ...

--- 8 kyu/Calculate Price Excluding VAT/app.js
+++ 8 kyu/Calculate Price Excluding VAT/app.js
@@ -31,3 +31,8 @@ function excludingVatPrice(price) {
         return Number((price / 1.15).toFixed(2));
     }
 }
+
+// solution-2:
+function excludingVatPrice(price) {
+    return (price == null) ? -1 : Number((price / 1.15).toFixed(2));
+}
