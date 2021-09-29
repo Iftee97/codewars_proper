cb7b871d7266ffb5cea11f5f4bb9614b8fee8ad7
Iftee1711402 <tawsifhaque97@gmail.com>
2021-09-29 09:06:53 +0600

    ...

--- 8 kyu/Did she say hallo/app.js
+++ 8 kyu/Did she say hallo/app.js
@@ -34,3 +34,23 @@ function validateHello(greetings) {
     let res = /(hello|ciao|salut|hallo|hola|ahoj|czesc)/i.test(greetings)
     return res
 }
+
+// solution-3:
+const hellos = {
+    hello: 'english',
+    ciao: 'italian',
+    salut: 'french',
+    hallo: 'german',
+    hola: 'spanish',
+    ahoj: 'czech republic',
+    czesc: 'polish',
+};
+
+const validateHello = greetings => {
+    for (key in hellos) {
+        if (greetings.toLowerCase().includes(key)) {
+            return true;
+        }
+    }
+    return false;
+};
