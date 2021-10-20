68daf0b83278c48f61f8e2a80cd2f647bd984ab8
Iftee1711402 <tawsifhaque97@gmail.com>
2021-10-21 05:48:00 +0600

    initial commit

---
+++ 8 kyu/Basic variable assignment/app.js
@@ -0,0 +1,18 @@
+/*
+
+Basic variable assignment
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+*/
+
+// // given code:
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+
+
+// solution:
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
