8d4d474100a463710e10b35a49003d588ba19a77
Iftee1711402 <tawsifhaque97@gmail.com>
2021-11-16 01:10:43 +0600

    initial commit

--- 8 kyu/altERnaTIng cAsE = ALTerNAtiNG CaSe/app.js
+++ 8 kyu/altERnaTIng cAsE = ALTerNAtiNG CaSe/app.js
@@ -31,3 +31,8 @@ String.prototype.toAlternatingCase = function () {
     }
     return A.join('');
 }
+
+// solution-2:
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
+}
