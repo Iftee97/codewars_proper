8f0c2ddb6265aba6ae15bbf992b5b3550b27ea9d
Iftee1711402 <tawsifhaque97@gmail.com>
2021-11-26 09:12:18 +0600

    initial commit

---
+++ 8 kyu/Training JS #1 create your first JS function and print Hello World!/app.js
@@ -0,0 +1,36 @@
+/*
+
+Training JS #1: create your first JS function and print "Hello World!"
+
+In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+Look at this example:
+
+function printWordToScreen(){
+  var somewords="This is an example."
+  console.log(somewords)
+}
+If we run this function, This is an example. will be seen on the screen. As you see, console.log() is an useful method help your work, you will become more and more like it.
+
+Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+*/
+
+// solution:
+function helloWorld() {
+    var str = 'Hello World!';
+    console.log(str);
+}
