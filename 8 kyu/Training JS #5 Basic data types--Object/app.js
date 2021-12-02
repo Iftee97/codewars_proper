dcf5c0d3e806e8b837613556878057621f0ed8ad
Iftee1711402 <tawsifhaque97@gmail.com>
2021-12-02 08:32:50 +0600

    initial commit

--- 8 kyu/Training JS #5 Basic data types--Object/app.js
+++ 8 kyu/Training JS #5 Basic data types--Object/app.js
@@ -21,8 +21,6 @@ and return a string like this:
 "This white dog has 4 legs."
 When you have finished the work, click "Run Tests" to see if your code is working properly.
 
-In the end, click "Submit" to submit your code pass this kata.
-
 */
 
 // solution:
