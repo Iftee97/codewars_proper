648ad1db6316771f863a7de2dd11eb01c99dac78
Iftee1711402 <tawsifhaque97@gmail.com>
2022-01-17 20:02:24 +0600

    added solution

--- 8 kyu/Calculate average/index.js
+++ 8 kyu/Calculate average/index.js
@@ -9,3 +9,17 @@ Note: Empty arrays should return 0.
 */
 
 // solution:
+function find_average(array) {
+    // your code here
+    let sum = 0;
+    let average = 0;
+
+    if (array.length === 0) {
+        return average = 0;
+    } else {
+        for (let num of array) {
+            sum += num;
+        }
+        return average = sum / array.length;
+    }
+}
