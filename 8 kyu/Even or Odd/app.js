cdaeed29100b452bff14745d02384daab9a36bf6
Iftee1711402 <tawsifhaque97@gmail.com>
2022-01-22 19:52:43 +0600

    added solution

---
+++ 8 kyu/Even or Odd/app.js
@@ -0,0 +1,12 @@
+/*
+
+Even or Odd
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+// solution
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+}
