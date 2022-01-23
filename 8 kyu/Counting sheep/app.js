ca6af8b104d4e7f7aaa09046360dd6a01ec7a12b
Iftee1711402 <tawsifhaque97@gmail.com>
2022-01-23 19:47:57 +0600

    added solution

--- 8 kyu/Counting sheep/app.js
+++ 8 kyu/Counting sheep/app.js
@@ -19,4 +19,13 @@ Hint: Don't forget to check for bad values like null/undefined
 */
 
 // solution:
+function countSheeps(arrayOfSheep) {
+    let count = 0;
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i]) {
+            count += 1
+        }
+    }
+    return count;
+}
 
