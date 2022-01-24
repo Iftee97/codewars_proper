94c1caf04a50fb9f18821e0797f2bd671e7659cd
Iftee1711402 <tawsifhaque97@gmail.com>
2022-01-24 19:45:04 +0600

    added solution

--- 8 kyu/Calculate BMI/app.js
+++ 8 kyu/Calculate BMI/app.js
@@ -14,3 +14,16 @@ if bmi > 30 return "Obese"
 
 */
 
+// solution:
+function bmi(weight, height) {
+    const bmi = (weight / height ** 2);
+    if (bmi <= 18.5) {
+        return 'Underweight'
+    } else if (bmi <= 25.0) {
+        return 'Normal'
+    } else if (bmi <= 30.0) {
+        return 'Overweight'
+    } else if (bmi > 30) {
+        return 'Obese'
+    }
+}
