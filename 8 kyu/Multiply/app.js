153a4db1cc39f7eab0fba16b72c402f4399b9071
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-03 19:21:32 +0600

    added solution - fixed bug

--- 8 kyu/Multiply/app.js
+++ 8 kyu/Multiply/app.js
@@ -6,3 +6,12 @@ This code does not execute properly. Try to figure out why.
 
 */
 
+// given code:
+function multiply(a, b) {
+    a * b;
+}
+
+// solution:
+function multiply(a, b) {
+    return a * b;
+}
