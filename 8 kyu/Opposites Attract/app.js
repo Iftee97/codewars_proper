ee182302ed811297caec59a2b5edfaa53dcce5b6
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-04 19:16:09 +0600

    added solution

--- 8 kyu/Opposites Attract/app.js
+++ 8 kyu/Opposites Attract/app.js
@@ -9,3 +9,12 @@ Write a function that will take the number of petals of each flower and return t
 */
 
 
+// solution:
+function lovefunc(flower1, flower2) {
+    if ((flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower2 % 2 === 0 && flower1 % 2 !== 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
