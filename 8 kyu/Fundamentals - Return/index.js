dcfc0107f64affa64a0accc87659160676edd62d
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-13 12:12:26 +0600

    added solution

--- 8 kyu/Fundamentals - Return/index.js
+++ 8 kyu/Fundamentals - Return/index.js
@@ -22,3 +22,27 @@ Note: All math operations will be: a (operation) b
 
 */
 
+// solution:
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function exponent(a, b) {
+    return a ** b
+}
+
+function subt(a, b) {
+    return a - b
+}
