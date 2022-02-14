feea3dc7e14a772d7e3cca26e6f2610034e29e7a
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-14 12:09:09 +0600

    added solution-2

--- 8 kyu/How good are you really/index.js
+++ 8 kyu/How good are you really/index.js
@@ -29,3 +29,14 @@ function betterThanAverage(classPoints, yourPoints) {
         return false;
     }
 }
+
+// solution-2:
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    var classAvg = 0;
+    for (var i = 0; i < classPoints.length; i++) {
+        classAvg += classPoints[i];
+    }
+    classAvg = classAvg / classPoints.length;
+    return yourPoints > classAvg;
+}
