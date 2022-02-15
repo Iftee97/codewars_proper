b4a7d3358acb25e8f84c13092949ca797f3b47b4
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-15 12:05:40 +0600

    added solution-2

--- 8 kyu/Check same case/index.js
+++ 8 kyu/Check same case/index.js
@@ -35,3 +35,14 @@ function sameCase(a, b) {
         return 0;
     }
 }
+
+// solution-2: (refactor)
+function sameCase(a, b) {
+    if (a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()) {
+        return -1
+    } else if (a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()) {
+        return 1
+    } else {
+        return 0
+    }
+}
