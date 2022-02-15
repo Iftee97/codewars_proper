6269716456a95959a4e71b3a7fdc683ea018bef1
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-15 12:04:47 +0600

    added solution

--- 8 kyu/Check same case/index.js
+++ 8 kyu/Check same case/index.js
@@ -20,3 +20,18 @@ Examples
 '0' and '?' returns -1
 
 */
+
+// solution:
+function sameCase(a, b) {
+    if (!(a.toUpperCase() != a.toLowerCase()) || !(b.toUpperCase() != b.toLowerCase())) {
+        return -1;
+    } else if ((a.charCodeAt(0) >= 65 && a.charCodeAt(0) <= 90) && (b.charCodeAt(0) >= 65 && b.charCodeAt(0) <= 90)) {
+        // a and b both uppercase
+        return 1;
+    } else if ((a.charCodeAt(0) >= 97 && a.charCodeAt(0) <= 122) && (b.charCodeAt(0) >= 97 && b.charCodeAt(0) <= 122)) {
+        // a and b both lowercase
+        return 1;
+    } else {
+        return 0;
+    }
+}
