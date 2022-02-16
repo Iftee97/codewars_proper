86cc050f94d0d16a07067701f7b73c5e4a922925
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-16 12:01:52 +0600

    added solution-2

--- 8 kyu/To square(root) or not to square(root)/index.js
+++ 8 kyu/To square(root) or not to square(root)/index.js
@@ -29,3 +29,6 @@ function squareOrSquareRoot(array) {
     }
     return newArray;
 }
+
+// solution-2:
+const squareOrSquareRoot = array => array.map(a => Math.sqrt(a) % 1 == 0 ? Math.sqrt(a) : (a * a));
