7101d16a92294489d01ce71fb714e8a3e7d6a9f3
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-20 11:43:51 +0600

    added solution

--- 8 kyu/String cleaning/app.js
+++ 8 kyu/String cleaning/app.js
@@ -13,3 +13,19 @@ Examples (input -> output):
 Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 
 */
+
+
+// solution:
+function stringClean(s) {
+    let newStr = '';
+    const numsArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    for (let i = 0; i < s.length; i++) {
+        if (numsArray.includes(s[i])) {
+            continue;
+        } else {
+            newStr += s[i];
+        }
+    }
+    return newStr;
+}
