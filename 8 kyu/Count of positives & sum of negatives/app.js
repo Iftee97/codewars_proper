31b4746c49dc2c3c6a505233ba5053559fde6493
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-21 11:41:05 +0600

    solution-2

--- 8 kyu/Count of positives & sum of negatives/app.js
+++ 8 kyu/Count of positives & sum of negatives/app.js
@@ -11,7 +11,7 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 */
 
-// solution:
+// solution-1:
 function countPositivesSumNegatives(arr) {
     let arrayToReturn = [];
     let sumOfNegatives = 0;
@@ -29,4 +29,34 @@ function countPositivesSumNegatives(arr) {
     }
     arrayToReturn.push(countOfPostives, sumOfNegatives);
     return arrayToReturn;
+}
+
+// solution-2:
+function countPositivesSumNegatives(input) {
+    var newArr = [];
+    var positiveNumber = 0;
+    var negativeNumber = 0;
+
+    // Validate Input
+    if (input === null || input.length === 0)
+        return newArr;
+
+    // Loop through array of Numbers 
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] == 0)
+            continue;
+
+        // Count positives
+        else if (input[i] > 0)
+            positiveNumber++;
+
+        // Sum negatives
+        else if (input[i] < 0)
+            negativeNumber += input[i];
+
+    }
+
+    // Prepare Output
+    newArr.push(positiveNumber, negativeNumber);
+    return newArr;
 }
