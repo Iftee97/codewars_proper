df04a6a3221990300aba95685e3459cbb8e6a65b
Iftee1711402 <tawsifhaque97@gmail.com>
2022-02-22 11:30:52 +0600

    initial commit

---
+++ 8 kyu/Remove String Spaces/app.js
@@ -0,0 +1,17 @@
+/*
+
+Remove String Spaces
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+*/
+
+// try-1
+function noSpace(x) {
+    return x.replace(/\s/g, '');
+}
+
+// try-2
+function noSpace(x) {
+    return x.split(' ').join('');
+}
