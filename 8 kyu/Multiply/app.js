fd74f3dcf5fb17dfb5b4c9ea5da61faa5402c990
Iftee1711402 <tawsifhaque97@gmail.com>
2022-03-23 04:12:33 +0600

    initial commit

--- 8 kyu/Multiply/app.js
+++ 8 kyu/Multiply/app.js
@@ -15,6 +15,10 @@ function multiply(a, b) {
     a * b;
 }
 
+
+// --------------------------------------------------------------------
+
+
 // solution:
 function multiply(a, b) {
     return a * b;
