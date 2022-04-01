84acc6a9b1911b5a84682915fa271a2f1007d7cb
Iftee1711402 <tawsifhaque97@gmail.com>
2022-04-02 03:54:45 +0600

    initial commit

--- 8 kyu/Find the smallest integer in the array/index.js
+++ 8 kyu/Find the smallest integer in the array/index.js
@@ -13,6 +13,10 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 
 */
 
+
+// --------------------------------------------------------------------
+
+
 // solution:
 function findSmallestInt(args) {
     return Math.min(...args)
