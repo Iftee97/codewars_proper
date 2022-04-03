9e4e86ab8dbd372f71a9ff24cb38915902174c66
Iftee1711402 <tawsifhaque97@gmail.com>
2022-04-04 03:53:37 +0600

    initial commit

--- 8 kyu/Find Maximum and Minimum Values of a List/index.js
+++ 8 kyu/Find Maximum and Minimum Values of a List/index.js
@@ -24,6 +24,10 @@ const min = function (list) {
     return Math.min(...list);
 }
 
+
+// --------------------------------------------------------------------
+
+
 const max = function (list) {
     return Math.max(...list);
 }
