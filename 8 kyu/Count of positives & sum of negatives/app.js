a87254ca7bafced9d8a477dd7c2fa36463998d2e
Iftee1711402 <tawsifhaque97@gmail.com>
2022-04-27 03:47:01 +0600

    initial commit

--- 8 kyu/Count of positives & sum of negatives/app.js
+++ 8 kyu/Count of positives & sum of negatives/app.js
@@ -11,6 +11,10 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 */
 
+
+// --------------------------------------------------------------------
+
+
 // solution-1:
 function countPositivesSumNegatives(arr) {
     let arrayToReturn = [];
