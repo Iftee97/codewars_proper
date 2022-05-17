17e758ee7dfc8cb02376a4fe9844265bb506edcd
Iftee1711402 <tawsifhaque97@gmail.com>
2022-05-18 04:23:32 +0600

    initial commit

--- 8 kyu/Is it a palindrome/index.js
+++ 8 kyu/Is it a palindrome/index.js
@@ -7,6 +7,29 @@ Write a function that checks if a given string (case insensitive) is a palindrom
 */
 
 // solution:
+// using for loop
+function checkPalindrome(string) {
+    // convert string to all lowercase letter:
+    string = string.toLowerCase();
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            console.log('It is not a palindrome');
+            return false;
+        }
+    }
+    console.log('It is a palindrome');
+    return true;
+}
+
+// solution-2:
+// using built-in functions
 function isPalindrome(string) {
     // convert string to all lowercase letter:
     string = string.toLowerCase();
@@ -29,3 +52,13 @@ function isPalindrome(string) {
         return false;
     }
 }
+
+// solution-3:
+// one liner
+const isPalindrome = (x) => {
+    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+}
+
+// solution-4:
+// one-liner on steroids
+const isPalindrome = x => x.toLowerCase().split('').reverse().join('') == x.toLowerCase();
