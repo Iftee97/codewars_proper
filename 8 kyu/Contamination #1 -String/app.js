3fbdb5e2e97bfe1096c8e48fba12537b179aa56b
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-07 16:43:35 +0600

    ...

---
+++ 8 kyu/Contamination #1 -String/app.js
@@ -0,0 +1,32 @@
+/*
+
+Contamination #1 -String-
+
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+
+*/
+
+
+// solution 1:
+function contamination(text, char) {
+  let strToReturn = '';
+  if (text === '' || char === '') {
+    return ''
+  }
+  for (let _ of text) {
+    strToReturn += char;
+  }
+  return strToReturn;
+}
