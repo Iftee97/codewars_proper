e09de67f56ef9e357dab05262004a25d861864d8
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-09 16:50:36 +0600

    ...

---
+++ 8 kyu/Enumerable Magic #3 - Does My List Include This/app.js
@@ -0,0 +1,12 @@
+/*
+
+Enumerable Magic #3 - Does My List Include This?
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+*/
+
+// solution 1:
+function doesMyListIncludeThis(list, item) {
+  return list.includes(item);
+}
